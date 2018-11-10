65416118c09289dc8b769551e59c6afadf5285ac
Davi Viana <davipviana@gmail.com>
2018-11-10 13:17:31 -0200

    Creating dashboard activity

---
+++ app/src/main/java/com/davipviana/usingroom/DashboardActivity.kt
@@ -0,0 +1,15 @@
+package com.davipviana.usingroom
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_dashboard.*
+
+class DashboardActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dashboard)
+    }
+}
