c733ad4972e34d582f8e26bfd4173bbfd4d9f381
Davi Viana <davipviana@gmail.com>
2018-11-10 13:33:04 -0200

    Creating students activity. Refactoring package organization.

--- app/src/main/java/com/davipviana/usingroom/activities/DashboardActivity.kt
+++ app/src/main/java/com/davipviana/usingroom/activities/DashboardActivity.kt
@@ -1,10 +1,8 @@
-package com.davipviana.usingroom
+package com.davipviana.usingroom.activities
 
-import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import kotlinx.android.synthetic.main.activity_dashboard.*
+import com.davipviana.usingroom.R
 
 class DashboardActivity : AppCompatActivity() {
 
---
+++ app/src/main/java/com/davipviana/usingroom/activities/StudentsActivity.kt
@@ -0,0 +1,13 @@
+package com.davipviana.usingroom.activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.davipviana.usingroom.R
+
+class StudentsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_students)
+    }
+}
